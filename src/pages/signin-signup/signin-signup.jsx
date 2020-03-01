038872aef1dfdac560ed569a71deeee8fdb24546
David N <bigair_1080@hotmail.com>
2020-03-01 17:43:14 +0100

    added custom signin form and button to site

---
+++ src/pages/signin-signup/signin-signup.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import SignIn from '../../components/signin/signin';
+
+import './signin-signup.scss';
+
+const SignInSignUpPage = () => (
+  <div className='signin-signup'>
+    <SignIn />
+  </div>
+);
+
+export default SignInSignUpPage;
