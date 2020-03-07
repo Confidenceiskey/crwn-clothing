0981a8e03d1d8b4f90c01477ba5d9c645b6e35a4
David N <bigair_1080@hotmail.com>
2020-03-07 20:56:25 +0100

    added firebase to project and authentication

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import Header from './components/header/header';
 import HomePage from './pages/homepage/homepage';
 import ShopPage from './pages/shop/shop';
@@ -21,10 +21,23 @@ class App extends Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
-
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapShot => {
+          this.setState({ 
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            },
+          });
+          console.log(this.state);
+        })
+      } else {
+        this.setState({
+          currentUser: userAuth
+        });
+      }
     });
   }
 
--- src/pages/signin-signup/signin-signup.jsx
+++ src/pages/signin-signup/signin-signup.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import SignIn from '../../components/signin/signin';
+import SignUp from '../../components/sign-up/sign-up';
 
 import './signin-signup.scss';
 
 const SignInSignUpPage = () => (
   <div className='signin-signup'>
     <SignIn />
+    <SignUp />
   </div>
 );
 
