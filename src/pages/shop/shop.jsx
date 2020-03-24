b24ae0641793c698722090bd5ce33cd2c0b0381c
David N <bigair_1080@hotmail.com>
2020-03-24 11:23:57 +0100

    modified redux shop files to use container components and refactored code

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -2,48 +2,30 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Route } from 'react-router-dom';
 
-import CollectionPage from '../collection/collection';
-import CollectionsOverview from '../../components/collections-overview/collections-overview';
-import WithSpinner from '../../components/with-spinner/with-spinner';
+import CollectionPageContainer from '../collection/collection.container';
+import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
 
-import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
-import { UpdateCollections } from '../../redux/shop/shop.actions';
-
-const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
-const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions';
 
 class ShopPage extends Component {
-  state = {
-    loading: true
-  };
-
-  unsubcribeFromSnapshot = null;
-
   componentDidMount() {
-    const { updateCollections } = this.props;
-    const collectionRef = firestore.collection('collections'); 
-    
-    collectionRef.get().then(snapshot => {
-      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-      updateCollections(collectionsMap);
-      this.setState({ loading: false });
-    });
+    const { fetchCollectionsStartAsync } = this.props;
+    fetchCollectionsStartAsync();
   } 
 
   render() {
     const { match } = this.props;
-    const { loading } = this.state;
     return (
       <div className="shop-page">
-        <Route exact path={`${match.path}`} render={(props) => <CollectionsOverviewWithSpinner isLoading={loading} {...props} />} />
-        <Route path={`${match.path}/:collectionId`} render={(props) => <CollectionPageWithSpinner isLoading={loading} {...props} />} />
+        <Route exact path={`${match.path}`} component={CollectionsOverviewContainer} />
+        <Route path={`${match.path}/:collectionId`} component={CollectionPageContainer} />} />
       </div>
     );
   }
-}  
+}
 
-const mapDispatchToProps = (dispatch) => ({
-  updateCollections: collectionsMap => dispatch(UpdateCollections(collectionsMap))
-})
+const mapDispatchToProps = dispatch => ({
+  fetchCollectionsStartAsync:  () => dispatch(fetchCollectionsStartAsync())
+});
 
 export default connect(null, mapDispatchToProps)(ShopPage);
--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -2,15 +2,29 @@ import shopActionTypes from './shop.types.js';
 
 const INITIAL_STATE = {
   collections: null,
+  errorMessage: '',
+  isFetching: false,
 };
 
 const shopReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case shopActionTypes.UPDATE_COLLECTIONS:
+    case shopActionTypes.FETCH_COLLECTIONS_START: 
+      return {
+        ...state,
+        isFetching: true
+      };
+    case shopActionTypes.FETCH_COLLECTIONS_SUCCESS:
       return {
         ...state,
         collections: action.payload,
-      }
+        isFetching: false
+      };
+    case shopActionTypes.FETCH_COLLECTIONS_FAILURE:
+      return {
+        ...state,
+        errorMessage: action.payload,
+        isFetching: false
+      };
     default:
       return state;
   }
